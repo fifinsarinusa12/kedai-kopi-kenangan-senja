3e644de0ac3cee811004c704f1ce57f5d892c2af
fifinsarinusa12 <fifinsarinusa12@gmail.com>
2026-08-30 16:12:21 +0700

    membuat website kedai kopi kenangan senja

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+// Ketika hamburger menu diklik
+document.querySelector("#hamburger-menu").onclick = (e) => {
+  e.preventDefault();
+  navbarNav.classList.toggle("active");
+};
+
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
